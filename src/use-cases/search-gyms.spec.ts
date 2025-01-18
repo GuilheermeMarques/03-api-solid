052e8b6ed2b7744e370cdcd3530e8fe34c48c61f
GuilheermeMarques <guiferreira_94@hotmail.com>
2025-01-18 10:20:00 -0300

    [feature] add search gyms use case

---
+++ src/use-cases/search-gyms.spec.ts
@@ -0,0 +1,62 @@
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { expect, describe, it, beforeEach } from 'vitest'
+import { SearchGymUseCase } from './search-gyms'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: SearchGymUseCase
+
+describe('Search Gyms Use Case', () => {
+  beforeEach(async () => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new SearchGymUseCase(gymsRepository)
+  })
+
+  it('should be able to search for gyms', async () => {
+    await gymsRepository.create({
+      title: 'JavaScript Gym',
+      descriptions: 'A melhor academia para programadores JavaScript',
+      phone: null,
+      latitude: -20.5381189,
+      longitude: -48.5687296,
+    })
+
+    await gymsRepository.create({
+      title: 'TypeScript Gym',
+      descriptions: 'A melhor academia para programadores TypeScript',
+      phone: null,
+      latitude: -20.5381189,
+      longitude: -48.5687296,
+    })
+
+    const { gyms } = await sut.execute({
+      query: 'JavaScript',
+      page: 1,
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'JavaScript Gym' })])
+  })
+
+  it('should be able to fetch paginated gyms search', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await gymsRepository.create({
+        title: `JavaScript Gym ${i}`,
+        descriptions: 'A melhor academia para programadores TypeScript',
+        phone: null,
+        latitude: -20.5381189,
+        longitude: -48.5687296,
+      })
+    }
+
+    const { gyms } = await sut.execute({
+      query: 'JavaScript',
+      page: 2,
+    })
+
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({ title: 'JavaScript Gym 21' }),
+      expect.objectContaining({ title: 'JavaScript Gym 22' }),
+    ])
+  })
+})
