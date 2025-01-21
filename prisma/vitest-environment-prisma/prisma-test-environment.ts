001a8c3ae8b5785888f48b98cc44bb44330bfc9c
GuilheermeMarques <guiferreira_94@hotmail.com>
2025-01-21 19:52:29 -0300

    [feature] create and delete schema on database before the tests

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,42 @@
+import 'dotenv/config'
+
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
 import { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDataBaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not defined')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    console.log('setup')
+    const schema = randomUUID()
+
+    const databaseURL = generateDataBaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync(`npx prisma migrate deploy --preview-feature`, {})
 
     return {
       async teardown() {
-        console.log('teardown')
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
